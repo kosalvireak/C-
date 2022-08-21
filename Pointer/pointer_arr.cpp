1f7c5be7a4223eacb379e17295c12ba256ebe7f9
Kosalvireak Khoeun <virakvary@gmail.com>
2022-08-21 22:00:20 +0700

    address and value of array from pointer

---
+++ Pointer/pointer_arr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int A[6] = {6, 3, 2, 8, 5, 1};
+int i;
+int *p = A;
+int main()
+{
+    for(int i = 0; i <= 5;i++){
+        cout<<"\nAddress: "<< p+i;
+        cout<<"\nValue: " <<*(p+i);
+    }   
+}
