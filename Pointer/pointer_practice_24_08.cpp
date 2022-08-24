eaa88f30040c7fbd7fb961660572f87c78f04a74
Kosalvireak Khoeun <virakvary@gmail.com>
2022-08-24 08:14:37 +0700

    add practice at rupp

---
+++ Pointer/pointer_practice_24_08.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int number = 20;
+    // int *pvalue;
+    // pvalue = &number;
+    // cout << number << endl; //call by value
+    // cout << pvalue << endl;
+    // cout << *pvalue;        //call by reference
+
+    char *pproverb1 = "Hello there";
+    const char *pproverb2 = "Hello there";
+    cout << pproverb1<<endl;
+    cout << pproverb2;
+}
