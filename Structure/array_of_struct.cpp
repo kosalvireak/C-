02e59f3f26168e1a0146e4788bc6ccb3777f48c9
Kosalvireak Khoeun <virakvary@gmail.com>
2022-09-07 12:01:07 +0700

    array of struct

---
+++ Structure/array_of_struct.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int i = 0;
+struct car
+{
+    string brand;
+    string color;
+    int plate;
+};
+struct car CAR[5];
+void get_value(int i){
+    
+    cout << "Enter brand: ";
+    cin >>CAR[i].brand;
+    
+    cout << "Enter color: ";
+    cin >>CAR[i].color;
+    
+    cout << "Enter year series: ";
+    cin >>CAR[i].plate;
+
+}
+void return_value(int i)
+{
+    cout << "brand: "<<CAR[i].brand<<endl;
+    cout << "color: "<<CAR[i].color<<endl;
+    cout << "plate: "<<CAR[i].plate<<endl;
+}
+int main(){
+    for (int i = 0; i < 5; i ++)
+    { 
+        get_value(i);
+    }
+   for (int i = 0; i < 5; i ++)
+    {
+        return_value(i);
+    }
+
+}
