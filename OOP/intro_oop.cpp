c4d801f44b11cb5cd0ebd82aea24de86291760e9
Kosalvireak Khoeun <virakvary@gmail.com>
2022-09-27 19:09:21 +0700

    commit

--- OOP/intro_oop.cpp
+++ OOP/intro_oop.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
-using namespace std;
-class Animal
+using std::cout;
+using std::endl;
+// Class to represent a box
+class Box
 {
-    private:
-        string strCountry;
-    public:
-        string name;
-        void getstrCountry()
-        {
-            return strCountry;
-        }
+public:
+    double length;
+    double width;
+    double height;
+    // Constructor
+    Box(double lengthValue, double widthValue, double heightValue)
+    {
+        cout << "Box constructor called" << endl;
+        length = lengthValue;
+        width = widthValue;
+        height = heightValue;
+    }
+    // Function to calculate the volume of a box
+    double volume()
+    {
+        return length * width * height;
+    }
+};
+int main()
+{
 
-}
-int main(){
-    Animal cat;
-    cat.name = "dodo";
-    cout <<cat.name;
-    // cout <<cat.getstrCountry;
+    Box firstBox(80, 50, 40);
+    // Calculate the volume of the box
+    cout << endl;
+    cout << "Size of first Box object is "
+         << firstBox.length << " by "
+         << firstBox.width << " by "
+         << firstBox.height
+         << endl;
+    cout << "Volume of first Box object is " << firstBox.volume()<< endl;
     return 0;
 }
