3a029833fbd9bccd39107c5bc80f72eeb839d0ec
Kosalvireak Khoeun <virakvary@gmail.com>
2022-10-16 19:29:07 +0700

    add final project

---
+++ Final_Project/finalproject.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+class Projector
+{
+public:
+    string P1 = "Projector 1:";
+    string P2 = "Projector 2:";
+    string P3 = "Projector 3:";
+    string P4 = "Projector 4:";
+    string P5 = "Projector 5:";
+    void show_data()
+    {
+        cout << "\n"
+             << P1;
+        cout << "\n"
+             << P2;
+        cout << "\n"
+             << P3;
+        cout << "\n"
+             << P4;
+        cout << "\n"
+             << P5;
+    }
+};
+int main()
+{
+    string me;
+    cout << "Enter your name: ";
+    cin >> me;
+    // cout << me;
+    Projector user;
+    while (true)
+    {
+        int choice;
+        cout << "--------------------------";
+        cout << "\nSelect one Projector below ";
+        cout << "\n--------------------------";
+        user.show_data();
+        cout << "\nEnter your choice: ";
+        string Book = "Booked to " + me;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            user.P1 += Book;
+            break;
+        case 2:
+            user.P2 += Book;
+            break;
+        case 3:
+            user.P3 += Book;
+            break;
+        case 4:
+            user.P4 += Book;
+            break;
+        case 5:
+            user.P5 += Book;
+            break;
+        case 6:
+            exit(0);
+            break;
+        default:
+            cout << "\nEnter correct choice";
+            exit(0);
+        }
+    }
+}
