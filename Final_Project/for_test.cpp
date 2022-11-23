0696ea316cb2a3653a5d876a89baece61d0dd27b
Kosalvireak Khoeun <virakvary@gmail.com>
2022-11-23 11:53:14 +0700

    commit

--- Final_Project/for_test.cpp
+++ Final_Project/for_test.cpp
@@ -1,13 +1,11 @@
-#include <cv.h>
-#include <highgui.h>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-using namespace cv;
 
-int main()
-{
-
-    Mat im = imread("IMG_1326.jpg", CV_LOAD_IMAGE_COLOR);
-    namedWindow("image");
-    imshow("image", im);
-    waitKey(0);
+int main() {
+    vector < int , int>vec ={
+        {1,2},{3,4},{5,6};
+        cout <<vec.at(1);
+    }
 }
